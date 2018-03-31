65051ee2b71b47ff93f2e2f875ef4e9a0c137f07
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-31 23:21:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,18 +7,18 @@ class School
   end
 
   def add_student (name, grade)
-    roster[grade] ||= []
+    @roster[grade] ||= []
     # It would make sense to sort entries on insert since sorted roster would
     # be a common requirement. But test requires names not be sorted at this time!
-    roster[grade] << name
+    @roster[grade] << name
   end
 
   def grade (n)
-    roster[n]
+    @roster[n]
   end
 
   def sort
     # storing sorted output back in roster so next request for sort will be fast
-    roster.each { | grade, names | names.sort! }
+    @roster.each { | grade, names | names.sort! }
   end
 end
